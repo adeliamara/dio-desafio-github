e874182faa8acc9930dde338f84b19963c0d0a50
adeliamara <adeliamara13@gmail.com>
2022-11-06 17:38:57 -0300

    inferfaces

--- introducao ao java/start-project/src/edu/introdutorio/interfaces/Calculadora.java
+++ introducao ao java/start-project/src/edu/introdutorio/interfaces/Calculadora.java
@@ -22,9 +22,9 @@ class Calculadora implements OperacaoMatematica {
         System.out.println("multiplicacao: " + operando1 * operando2);
     }
 
-//    @Override
-//    public void divisao(double operando1, double operando2) {
-//       System.out.println("multiplicacao: " + operando1 / operando2);
-//    }
+    @Override
+     public void divisao(double operando1, double operando2) {
+     System.out.println("multiplicacao: " + operando1 / operando2);
+ }
 
 }
