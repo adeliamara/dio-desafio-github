0135c02d1d1b9032dadfe5fe295f1664117ebcb8
adeliamara <adeliamara13@gmail.com>
2022-11-06 17:33:07 -0300

    codigo interfaces

---
+++ introducao ao java/start-project/src/edu/introdutorio/interfaces/OperacaoMatematica.java
@@ -0,0 +1,17 @@
+package edu.introdutorio.interfaces;
+
+
+/**
+ * Interface de exemplo para o exercício da Aula 4 de Orientação a Objetos.
+ */
+
+public interface OperacaoMatematica {
+
+    void soma(double operando1, double operando2);
+
+    void subtracao(double operando1, double operando2);
+
+    void multiplicacao(double operando1, double operando2);
+
+    void divisao(double operando1, double operando2);
+}
